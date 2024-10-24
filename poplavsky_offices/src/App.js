b737a3e4dd571066794883759621ad79a6fdd79d
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-10-24 16:32:09 -0300

    btn navbar

--- poplavsky_offices/src/App.js
+++ poplavsky_offices/src/App.js
@@ -7,6 +7,7 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
+
 import Home from './sections/Home/Home';
 import AboutUs from './sections/AboutUs/AboutUs';
 import Services from './sections/Services/Services';
--- poplavsky_offices/src/components/NavBar/NavBar.jsx
+++ poplavsky_offices/src/components/NavBar/NavBar.jsx
@@ -12,13 +12,13 @@ export default function NavBar() {
   return (
     <div className="NavBar grid grid-nogutter nested-grid h-full p-2 justify-content-between">
       <div className='col-2 bg-bluegray-400 border-round flex justify-content-center align-items-center'>Logo</div>
-      <div className='col-5 bg-bluegray-400 border-round grid grid-nogutter justify-content-between align-items-center px-2'>
-        <Button label="Home" outlined onClick={() => navigate('/')} className='text-yellow-500 py-2 px-4' />
-        <Button label="About Us" outlined onClick={() => navigate('/about-us')} className='text-yellow-500 py-2 px-4' />
-        <Button label="Services" outlined onClick={() => navigate('/services')} className='text-yellow-500 py-2 px-4' />
+      <div className='col-5 bg-bluegray-400 border-round grid grid-nogutter justify-content-center align-items-center px-2 gap-4'>
+        <Button label="Home" size='small' outlined onClick={() => navigate('/')} className='text-yellow-500 py-2 px-4' />
+        <Button label="About Us" size='small' outlined onClick={() => navigate('/about-us')} className='text-yellow-500 py-2 px-4' />
+        <Button label="Services" size='small' outlined onClick={() => navigate('/services')} className='text-yellow-500 py-2 px-4' />
       </div>
       <div className='col-2 bg-bluegray-400 border-round flex justify-content-center align-items-center'>
-        <Button label="Contact" outlined onClick={() => navigate('/')} className='text-yellow-500 py-2 px-4' />
+        <Button label="Contact" size='small' outlined onClick={() => navigate('/')} className='text-yellow-500 py-2 px-4' />
       </div>
     </div>
   );
