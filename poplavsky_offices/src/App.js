12b33f981d60afb07a7512483ee0332ef18a4b83
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-10-24 11:11:07 -0300

    Imports css libraris | estructuras

--- poplavsky_offices/src/App.js
+++ poplavsky_offices/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
+/* PRIME REACT */
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
 import Home from './sections/Home/Home';
 import AboutUs from './sections/AboutUs/AboutUs';
 import Services from './sections/Services/Services';
--- poplavsky_offices/src/sections/Home/Home.jsx
+++ poplavsky_offices/src/sections/Home/Home.jsx
@@ -1,11 +1,17 @@
 
 import React from 'react';
 import './Home.css';
+import NavBar from '../../components/NavBar/NavBar';
 
-export default function Home () {
+export default function Home() {
   return (
-    <div className="Home">
-      <h1>Home Component</h1>
+    <div className="Home grid grid-nogutter nested-grid p-3">
+      <nav className="col-12 bg-blue-400">
+        <NavBar />
+      </nav>
+      <section className='col-12'>
+        <h1>Home Component</h1>
+      </section>
     </div>
   );
 };
