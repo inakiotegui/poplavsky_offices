e9855cdf12a222f43bfaa6af9a0cea76973d1c42
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-10-25 11:00:12 -0300

    add seciton summary services | tarjetas

--- poplavsky_offices/src/components/services/SummaryServices/SummaryServices.jsx
+++ poplavsky_offices/src/components/services/SummaryServices/SummaryServices.jsx
@@ -2,12 +2,36 @@
 import React from 'react';
 import './SummaryServices.css';
 
+import icon1 from '../../../assets/icons/icons-services/icon-1.png';
+import icon2 from '../../../assets/icons/icons-services/icon-2.png';
+import icon3 from '../../../assets/icons/icons-services/icon-3.png';
+import SummaryServicesCards from '../SummaryServicesCards/SummaryServicesCards';
+
 export default function SummaryServices() {
+
+  const services = [
+    {
+      icon: icon1,
+      description: 'Asistencia en la apertura de Cuentas Bancaria Corporativas',
+      link: '/services/1',
+    },
+    {
+      icon: icon2,
+      description: 'Constitución de Empresas en la Zona Franca Más Adecuada para Su Negocio',
+      link: '/services/2',
+    },
+    {
+      icon: icon3,
+      description: 'Realización de Estudios de Mercado para Evaluar Oportunidades Comerciales',
+      link: '/services/3',
+    }
+  ]
+
   return (
     <div className="SummaryServices grid grid-nogutter nested-grid bg-bluegray-800 border-round gap-8 p-3 relative fadein animation-duration-1000 animation-iteration-1 animation-ease-in-out">
-      <div className='col bg-bluegray-400 border-round h-10rem'></div>
-      <div className='col bg-bluegray-400 border-round h-10rem'></div>
-      <div className='col bg-bluegray-400 border-round h-10rem'></div>
+      {services.map((service, index) =>
+        <SummaryServicesCards service={service} />
+      )}
     </div>
   );
 };
--- poplavsky_offices/src/components/services/SummaryServicesCards/SummaryServicesCards.jsx
+++ poplavsky_offices/src/components/services/SummaryServicesCards/SummaryServicesCards.jsx
@@ -2,10 +2,21 @@
 import React from 'react';
 import './SummaryServicesCards.css';
 
-export default function SummaryServicesCards () {
+export default function SummaryServicesCards({ service }) {
   return (
-    <div className="SummaryServicesCards">
-      <h1>SummaryServicesCards Component</h1>
+    <div className='SummaryServicesCards col bg-bluegray-400 border-round grid grid-nogutter nested-grid gap-3 p-3 border-white hover:border-js-yellow border-1 relative cursor-pointer'>
+      <i className='pi pi-arrow-up-right absolute text-xl text-gray-200' style={{ right: '10px', top: '10px' }}></i>
+
+      <div className='col-12 flex justify-content-center align-items-center'
+        style={{ height: 'fit-content' }}
+      >
+        <img src={service.icon} alt="icon" className='w-4rem h-4rem' />
+      </div>
+      <div className='col-12 bg-bluegray-200 border-round h-4rem flex justify-content-center align-items-center'>
+        <p className='m-0 text-center text-white'>
+          {service.description}
+        </p>
+      </div>
     </div>
   );
 };
