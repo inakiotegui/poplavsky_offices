ff9d9ed712ad16340f64f45cd5110b54a736a6d0
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-11-04 00:16:57 -0300

    responsive mobile

--- poplavsky_offices/src/components/Banner/Banner.jsx
+++ poplavsky_offices/src/components/Banner/Banner.jsx
@@ -9,15 +9,15 @@ export default function Banner({ backgroundLogo, backgroundImage, backgroundGrad
             }}>
 
             <div
-                className="Banner"
+                className="BannerMobile md:Banner"
                 style={{
                     backgroundImage: `url(${backgroundImage}), url(${backgroundLogo})`,
                 }}
             >
-                <div className="grid grid-nogutter nested-grid justify-content-center align-content-center h-22rem scalein animation-duration-300 animation-iteration-1">
-                    <div className="w-10 text-center">
+                <div className="grid grid-nogutter nested-grid justify-content-center align-content-center h-18rem md:h-22rem scalein animation-duration-300 animation-iteration-1">
+                    <div className="w-full md:w-10  md:text-center">
                         <h2 className="text-white text-2xl md:text-4xl m-2">{title}</h2>
-                        <h4 className="m-0 text-xs md:text-lg w-full font-normal text-gray-500">{subtitle}</h4>
+                        <h4 className="m-0 text-xs md:text-lg w-full font-normal text-gray-500 m-2">{subtitle}</h4>
                     </div>
                 </div>
             </div>
