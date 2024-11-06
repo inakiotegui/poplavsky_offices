8f99964f4c19e03743935f6db007d7a3ad219c37
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-11-05 22:58:34 -0300

    Add total summary services | refactorizacion a carrusel

--- poplavsky_offices/src/components/services/SummaryServices/SummaryServices.jsx
+++ poplavsky_offices/src/components/services/SummaryServices/SummaryServices.jsx
@@ -1,37 +1,59 @@
 
 import React from 'react';
 import './SummaryServices.css';
-
-import icon1 from '../../../assets/icons/icons-services/icon-1.png';
-import icon2 from '../../../assets/icons/icons-services/icon-2.png';
-import icon3 from '../../../assets/icons/icons-services/icon-3.png';
+import { Carousel } from "primereact/carousel";
+import servicesInfo from "../../../utils/jsons/ServicesInfo/SummaryServices";
 import SummaryServicesCards from '../SummaryServicesCards/SummaryServicesCards';
 
 export default function SummaryServices() {
 
-  const services = [
+  const responsiveOptions = [
     {
-      icon: icon1,
-      description: 'Assistance with Corporate Bank Account Opening',
-      link: '/services/1',
+      breakpoint: "1400px",
+      numVisible: 3,
+      numScroll: 1,
     },
     {
-      icon: icon2,
-      description: 'Company Formation in the Most Suitable Free Zone for Your Business',
-      link: '/services/2',
+      breakpoint: "1199px",
+      numVisible: 3,
+      numScroll: 1,
     },
     {
-      icon: icon3,
-      description: 'Conducting Market Studies to Evaluate Business Opportunities',
-      link: '/services/3',
-    }
-  ]
+      breakpoint: "767px",
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "575px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
+
+  const summaryServiceTemplate = (item) => {
+    return (
+      <div className='mx-2 md:mx-6'>
+        <SummaryServicesCards service={item} />
+      </div>
+    );
+  };
+
 
   return (
     <div className="SummaryServices grid grid-nogutter nested-grid  border-round gap-4 md:gap-8 p-3 relative fadein animation-duration-1000 animation-iteration-1 animation-ease-in-out">
-      {services.map((service, index) =>
-        <SummaryServicesCards service={service} />
-      )}
+      <Carousel
+        value={servicesInfo}
+        numVisible={3}
+        numScroll={3}
+        responsiveOptions={responsiveOptions}
+        showIndicators={false}
+        showNavigators={false}
+        className="w-full"
+        contentClassName=''
+        circular
+        autoplayInterval={5000}
+        itemTemplate={summaryServiceTemplate}
+      />
     </div>
   );
 };
---
+++ poplavsky_offices/src/utils/jsons/ServicesInfo/SummaryServices.js
@@ -0,0 +1,43 @@
+import icon1 from '../../../assets/icons/icons-services/icon-1.png';
+import icon2 from '../../../assets/icons/icons-services/icon-2.png';
+import icon3 from '../../../assets/icons/icons-services/icon-3.png';
+
+const services = [
+    {
+        icon: icon1,
+        description: 'Company Formation with Customized Solutions for Success in the UAE',
+        link: '/services/1',
+    },
+    {
+        icon: icon2,
+        description: 'Comprehensive UAE Residency Visa Assistance',
+        link: '/services/2',
+    },
+    {
+        icon: icon3,
+        description: 'Corporate Bank Account Assistance with Streamlined Processes',
+        link: '/services/3',
+    },
+    {
+        icon: icon1,
+        description: 'Expert Market Research for Maximizing Business Opportunities',
+        link: '/services/4',
+    },
+    {
+        icon: icon2,
+        description: 'Commercial Representation to Connect You with Key Distributors in the UAE',
+        link: '/services/5',
+    },
+    {
+        icon: icon3,
+        description: 'Professional Debt Collection Services to Secure Outstanding Payments',
+        link: '/services/6',
+    },
+    {
+        icon: icon1,
+        description: 'Tax Consultancy for Compliance and Optimization',
+        link: '/services/7',
+    }
+];
+
+export default services;
