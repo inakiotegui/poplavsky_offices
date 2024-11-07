fe72dc5875200eb10f10b9ebb2de66ed31557235
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-11-07 13:25:23 -0300

    add envio de mail

--- poplavsky_offices/src/sections/ContactUs/ContactUs.jsx
+++ poplavsky_offices/src/sections/ContactUs/ContactUs.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from 'emailjs-com';
 import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Button } from 'primereact/button';
 import './ContactUs.css';
 import NavBar from '../../components/NavBar/NavBar';
 import CustomDivider from '../../utils/Divider/CustomDivider/CustomDivider';
+import { Toast } from 'primereact/toast';
 
 export default function ContactUs() {
   const [formData, setFormData] = useState({
@@ -12,18 +14,16 @@ export default function ContactUs() {
     email: '',
     message: ''
   });
-
   const [isEmailValid, setIsEmailValid] = useState(true);
+  const toast = useRef(null);
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
-    // Validar formato de email si el campo es 'email'
     if (name === 'email') {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       setIsEmailValid(emailRegex.test(value));
     }
-
     setFormData({
       ...formData,
       [name]: value
@@ -32,15 +32,32 @@ export default function ContactUs() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
-    // Aquí podrías manejar el envío de datos (ej: enviar a un servidor)
+    if (!isFormComplete) return;
+
+    emailjs.send(
+      'service_bzybkt4',
+      'template_ryg77db',
+      {
+        user_name: formData.fullName,
+        user_email: formData.email,
+        message: formData.message,
+      },
+      'iLrqW7rFdKP20vKjx'
+    )
+      .then(response => {
+        toast.current.show({ severity: 'info', summary: 'Info', detail: 'Email sent successfully' });
+        setFormData({ fullName: '', email: '', message: '' });
+      })
+      .catch(err => {
+        toast.current.show({ severity: 'error', summary: 'Error', detail: 'Failed to send email' });
+      });
   };
 
   const isFormComplete = formData.fullName && formData.email && formData.message && isEmailValid;
 
   const handleToInsta = () => {
     window.open('https://www.instagram.com/popladubai/', '_blank');
-  }
+  };
 
   return (
     <div className="ContactUs grid grid-nogutter nested-grid justify-content-center align-content-center gap-1"
@@ -51,20 +68,17 @@ export default function ContactUs() {
       </nav>
 
       <div className='col-10 grid grid-nogutter nested-grid gap-3'>
-
-
         <section className="col-12 md:col-5 flex align-items-center">
           <div className="w-full px-4 md:px-6 grid grid-nogutter nested-grid justify-content-start align-items-center border-round">
             <p className='w-full flex gap-3 align-items-center text-white'><i className='text-js-yellow pi pi-map-marker'></i>Opus Tower, C101, Business Bay. Dubai, UAE</p>
             <p className='w-full flex gap-3 align-items-center text-white'><i className='text-js-yellow pi pi-phone'></i>+971 585661300</p>
             <p className='w-full flex gap-3 align-items-center text-white'><i className='text-js-yellow pi pi-envelope'></i>patricio@poplavskydubaioffices.com</p>
-            <p className='w-full flex gap-3 align-items-center text-white cursor-pointer' onClick={() => handleToInsta()}><i className='text-js-yellow pi pi-instagram'></i>Follow us @poplavsky</p>
+            <p className='w-full flex gap-3 align-items-center text-white cursor-pointer' onClick={handleToInsta}><i className='text-js-yellow pi pi-instagram'></i>Follow us @poplavsky</p>
           </div>
         </section>
 
-
         <section className="col-12 md:col border-round bg-trl-blue border-1 border-js-yellow">
-          <div className="w-full px-4 md:px-6 grid grid-nogutter nested-grid justify-content-start align-items-center  border-round">
+          <div className="w-full px-4 md:px-6 grid grid-nogutter nested-grid justify-content-start align-items-center border-round">
             <div>
               <CustomDivider />
               <h2 className="text-white text-2xl md:text-4xl m-2">Contact Us</h2>
@@ -117,9 +131,8 @@ export default function ContactUs() {
             </div>
           </form>
         </section>
-
       </div>
-
+      <Toast ref={toast} />
     </div>
   );
 }
