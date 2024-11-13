73800cf50a30b5b9e8780e675de08ffe16671b5d
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-11-13 11:13:04 -0300

    wsp en contato

--- poplavsky_offices/src/App.js
+++ poplavsky_offices/src/App.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
 
 /* PRIME REACT */
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
+import Home from "./sections/Home/Home";
+import AboutUs from "./sections/AboutUs/AboutUs";
+import Services from "./sections/Services/Services";
+import ContactUs from "./sections/ContactUs/ContactUs";
 
-import Home from './sections/Home/Home';
-import AboutUs from './sections/AboutUs/AboutUs';
-import Services from './sections/Services/Services';
-import ContactUs from './sections/ContactUs/ContactUs';
-
-import wsp from "./assets/icons/general/wsp.png"
+import wsp from "./assets/icons/general/wsp.png";
 
 function App() {
-  window.addEventListener("scroll", function () {
+  const location = useLocation();
+
+  useEffect(() => {
     const whatsappButton = document.querySelector(".whatsapp-button");
-    if (window.scrollY > 100) {
+
+    if (location.pathname === "/contact-us") {
+      whatsappButton.classList.add("show");
+    } else if (window.scrollY > 100) {
       whatsappButton.classList.add("show");
     } else {
       whatsappButton.classList.remove("show");
     }
-  });
+
+    const handleScroll = () => {
+      if (window.scrollY > 100 || location.pathname === "/contact-us") {
+        whatsappButton.classList.add("show");
+      } else {
+        whatsappButton.classList.remove("show");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [location]);
 
   return (
     <div className="App">
@@ -35,13 +51,13 @@ function App() {
       </Routes>
       <a
         href="https://wa.me/971585661300"
-        class="whatsapp-button"
+        className="whatsapp-button"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img src={wsp} alt="WhatsApp" />
       </a>
-    </div >
+    </div>
   );
 }
 
