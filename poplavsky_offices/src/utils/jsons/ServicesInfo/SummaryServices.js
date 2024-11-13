09e7590e57b7a6e233801447e8254590f176139f
Bufalito <93780179+Bufalito@users.noreply.github.com>
2024-11-13 10:51:16 -0300

    Add icons referenciales

--- poplavsky_offices/src/utils/jsons/ServicesInfo/SummaryServices.js
+++ poplavsky_offices/src/utils/jsons/ServicesInfo/SummaryServices.js
@@ -1,50 +1,57 @@
-import icon1 from '../../../assets/icons/icons-services/icon-1.png';
-import icon2 from '../../../assets/icons/icons-services/icon-2.png';
-import icon3 from '../../../assets/icons/icons-services/icon-3.png';
+import icon1 from "../../../assets/icons/icons-services/icon-1.png";
+import icon2 from "../../../assets/icons/icons-services/icon-2.png";
+import icon3 from "../../../assets/icons/icons-services/icon-3.png";
+import icon4 from "../../../assets/icons/icons-services/icon-4.png";
+import icon5 from "../../../assets/icons/icons-services/icon-5.png";
+import icon6 from "../../../assets/icons/icons-services/icon-6.png";
+import icon7 from "../../../assets/icons/icons-services/icon-7.png";
 
 const services = [
-    {
-        icon: icon1,
-        description: 'Company Formation with Customized Solutions for Success in the UAE',
-        path: '/services',
-        numero: "serv01"
-    },
-    {
-        icon: icon2,
-        description: 'Comprehensive UAE Residency Visa Assistance',
-        path: '/services',
-        numero: "serv02"
-    },
-    {
-        icon: icon3,
-        description: 'Corporate Bank Account Assistance with Streamlined Processes',
-        path: '/services',
-        numero: "serv03"
-    },
-    {
-        icon: icon1,
-        description: 'Expert Market Research for Maximizing Business Opportunities',
-        path: '/services',
-        numero: "serv04"
-    },
-    {
-        icon: icon2,
-        description: 'Commercial Representation to Connect You with Key Distributors in the UAE',
-        path: '/services',
-        numero: "serv05"
-    },
-    {
-        icon: icon3,
-        description: 'Professional Debt Collection Services to Secure Outstanding Payments',
-        path: '/services',
-        numero: "serv06"
-    },
-    {
-        icon: icon1,
-        description: 'Tax Consultancy for Compliance and Optimization',
-        path: '/services',
-        numero: "serv07"
-    }
+  {
+    icon: icon1,
+    description:
+      "Company Formation with Customized Solutions for Success in the UAE",
+    path: "/services",
+    numero: "serv01",
+  },
+  {
+    icon: icon2,
+    description: "Comprehensive UAE Residency Visa Assistance",
+    path: "/services",
+    numero: "serv02",
+  },
+  {
+    icon: icon3,
+    description: "Corporate Bank Account Assistance with Streamlined Processes",
+    path: "/services",
+    numero: "serv03",
+  },
+  {
+    icon: icon4,
+    description: "Expert Market Research for Maximizing Business Opportunities",
+    path: "/services",
+    numero: "serv04",
+  },
+  {
+    icon: icon5,
+    description:
+      "Commercial Representation to Connect You with Key Distributors in the UAE",
+    path: "/services",
+    numero: "serv05",
+  },
+  {
+    icon: icon6,
+    description:
+      "Professional Debt Collection Services to Secure Outstanding Payments",
+    path: "/services",
+    numero: "serv06",
+  },
+  {
+    icon: icon7,
+    description: "Tax Consultancy for Compliance and Optimization",
+    path: "/services",
+    numero: "serv07",
+  },
 ];
 
 export default services;
