eb71b2787faf79994ec9cf796885615d29e485d4
Bufalito <pablobojanich1@gmail.com>
2024-12-17 09:56:18 -0300

    Fix reestructuracion general

--- poplavsky_offices/src/sections/AboutUs/AboutUs.jsx
+++ poplavsky_offices/src/sections/AboutUs/AboutUs.jsx
@@ -11,14 +11,16 @@ import BannerAboutUs from "../../components/AboutUs/BannerAboutUs/BannerAboutUs"
 import OurTeam from "../../components/AboutUs/OurTeam/OurTeam";
 import Map from "../../components/Map/Map";
 
+import CustomDivider from "../../utils/Divider/CustomDivider/CustomDivider";
+import WhyChooseUs from "../../components/Home/WhyChooseUs/WhyChooseUs";
+
 export default function AboutUs() {
   const bannerRef = useRef(null);
-  const infoRef = useRef(null);
   const finalRef = useRef(null);
   const teamRef = useRef(null);
   const location = useLocation();
 
-  const refs = { bannerRef, infoRef, finalRef, teamRef };
+  const refs = { bannerRef, finalRef, teamRef };
 
   const scrollToSection = (ref) => {
     ref?.current?.scrollIntoView({
@@ -44,14 +46,45 @@ export default function AboutUs() {
       <section ref={bannerRef} className="col-12">
         <BannerAboutUs />
       </section>
-      <section ref={infoRef} className="col-12">
-        <InfoAboutUs />
+
+      <section className="col-12 my-6">
+        <div className="grid grid-nogutter gap-3 nested-grid justify-content-start align-items-center  border-round pb-2">
+          <div className="col-12 md:col-6 px-4 md:px-6">
+            <CustomDivider />
+            <h2 className="text-white text-2xl md:text-4xl m m-2">
+              Our Vision
+            </h2>
+            <h4 className="m-0 text-xs md:text-lg w-full font-normal text-gray-500">
+              To be the leading partner for entrepreneurs expanding to the UAE,
+              known for unlocking global opportunities and delivering excellence
+              in advisory services.
+            </h4>
+          </div>
+
+          <div className="col-12 md:col px-4 md:px-6">
+            <CustomDivider />
+            <h2 className="text-white text-2xl md:text-4xl m m-2">
+              Our Mission
+            </h2>
+            <h4 className="m-0 text-xs md:text-lg w-full font-normal text-gray-500">
+              To empower entrepreneurs in their expansion to the UAE, providing
+              comprehensive support at every step to ensure a successful and
+              seamless market entry.
+            </h4>
+          </div>
+        </div>
       </section>
-      <section ref={finalRef}>
-        <BloqueFinalAboutUs />
+
+      <section className="col-12">
+        <WhyChooseUs />
       </section>
+
       <section ref={teamRef}>
         <OurTeam />
+      </section>
+
+      <section ref={finalRef}>
+        <BloqueFinalAboutUs />
       </section>
       <section className="col-12">
         <Map />
--- poplavsky_offices/src/sections/Home/Home.jsx
+++ poplavsky_offices/src/sections/Home/Home.jsx
@@ -11,14 +11,19 @@ import FAQs from "../../components/Home/FAQs/FAQs";
 import FooterContacto from "../../components/FooterContacto/FooterContacto";
 import FooterEnlaces from "../../components/FooterEnlaces/FooterEnlaces";
 
+import InfoAboutUs from "../../components/AboutUs/InfoAboutUs";
+import BloqueFinalAboutUs from "../../components/BloqueFinalAboutUs/InfoFinalAboutUs";
+
 export default function Home() {
   const heroRef = useRef(null);
   const whyUs = useRef(null);
   const prensaRef = useRef(null);
+  const infoRef = useRef(null);
+  const finalRef = useRef(null);
   const faqsRef = useRef(null);
   const location = useLocation();
 
-  const refs = { heroRef, whyUs, prensaRef, faqsRef };
+  const refs = { heroRef, whyUs, prensaRef, faqsRef, infoRef, finalRef };
 
   const scrollToSection = (ref) => {
     ref?.current?.scrollIntoView({
@@ -47,6 +52,14 @@ export default function Home() {
 
       <section className="col-12 mb-3 md:mb-8">
         <SummaryServices />
+      </section>
+
+      <section ref={infoRef} className="col-12">
+        <InfoAboutUs />
+      </section>
+
+      <section ref={finalRef}>
+        <BloqueFinalAboutUs />
       </section>
 
       <section ref={whyUs} className="col-12 mb-3 md:mb-8">
